e1a1e2b2aa55baede7782c9313b572113a7659a9
Wojtyla <144253449+RaplesWojtyla@users.noreply.github.com>
2025-04-18 22:45:05 +0700

    fix: bg

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Register from "./components/Register"
 const App = () => {
 	return (
 		<div className="min-h-screen flex font-poppins">
-			<div className="w-[60%] min-h-screen bg-[url('bg.png')] bg-cover bg-center">
+			<div className="w-[60%] min-h-screen bg-[url('./bg.png')] bg-cover bg-center">
 				<div className="py-6 px-8 h-full flex flex-col justify-between">
 					<p className="text-3xl font-bold bg-gradient-to-br from-cyan-400 to-blue-800 bg-clip-text text-transparent"><span className="text-4xl">W</span>ojtyla</p>
 
